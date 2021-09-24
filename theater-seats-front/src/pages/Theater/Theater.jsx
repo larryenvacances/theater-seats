e9ef62b7020c6756c86c18cdf29d22cce6fe6404
Larry Huot <huot.laurent11@gmail.com>
2021-09-24 14:55:01 -0400

    add theater component

--- theater-seats-front/src/pages/Theater/Theater.jsx
+++ theater-seats-front/src/pages/Theater/Theater.jsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 
+import './Theater.css';
+
 export default function Theater() {
+  let floor = Array(4)
+    .fill(false)
+    .map((row) => new Array(4).fill(false));
+
   return (
-    <div>
-      <h1>Potato</h1>
+    <div className="Floor">
+      <table>
+        {floor.map((row, rowIndex) => {
+          return (
+            <tr>
+              {row.map((col, colIndex) => {
+                return (
+                  <td className="Seat">
+                    {rowIndex}, {colIndex}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
