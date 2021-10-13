99d1ea12b5b26bb9bb0a7df8e8c0f7f742e9d1c1
Larry Huot <huot.laurent11@gmail.com>
2021-10-13 13:34:04 -0400

    deleted frontend. hate it anyway.

--- theater-seats-front/src/hooks/useAppContext.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { AppContext } from '../context/AppContextProvider';
-
-export default () => {
-  const value = useContext(AppContext);
-
-  return value;
-};
